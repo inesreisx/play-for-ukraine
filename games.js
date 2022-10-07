2381c9963debc3cf8fd3d2dbe4f353f5eb52e327
João <41347100+Sefhriloff@users.noreply.github.com>
2022-10-07 23:45:40 +0100

    Create games.js

---
+++ games.js
@@ -0,0 +1,18 @@
+_games = [
+    { name: 'Cars Eats Car Evil Cars', img: 'https://smokoko.com/upload/content/nIM7Oki0.jpg', url: 'https://www.crazygames.com/game/car-eats-car-evil-cars' },
+    { name: 'Ukrainian games mylanguages.org', img: 'http://127.0.0.1:5500/Ukrainian-games-mylanguages.org.png', url: 'https://mylanguages.org/ukrainian_games.php' },
+    { name: 'Cinque Terre', img: 'https://ain.capital/wp-content/uploads/2022/05/photo_2022-05-17_12-33-32-1.jpg', url: 'https://ua.adact.me/help' },
+    { name: 'Slava Ukraini game', img: 'https://img.itch.zone/aW1nLzg5OTQ1NjUucG5n/315x250%23c/RRY18k.png', url: 'https://africacrossgames.itch.io/slava-ukraini' },
+    { name: 'Colors of Motanka', img: 'https://img.itch.zone/aW1nLzk0Mzg5MTMuanBn/315x250%23c/f6vsYi.jpg', url: 'https://xardox.itch.io/colors-of-motanka' },
+    { name: 'Power and Chaos', img: 'https://img.itch.zone/aW1nLzk0NjQ5NzAuanBn/315x250%23c/ri2pXc.jpg', url: 'https://ukrplay.itch.io/power-and-chaos' },
+    { name: 'Hope dies last', img: 'https://img.itch.zone/aW1nLzg1NTAyNjcucG5n/315x250%23c/Idw76m.png', url: 'https://bardon.itch.io/hope-dies-last' },
+    { name: 'Gourmet Supply', img: 'https://img.itch.zone/aW1nLzg5Mzc1ODAucG5n/315x250%23c/xREDvm.png', url: 'https://eva-enta.itch.io/gourmet-supply=' },
+    { name: 'Musk vs Putin game', img: 'https://odessa-journal.com/wp-content/uploads/2022/04/muskvsputin.jpg', url: 'https://musk-putin.com/' },
+    { name: 'Hryvnia Stories', img: 'https://img.itch.zone/aW1nLzk0NDcxNDAucG5n/315x250%23c/M%2FVef5.png', url: 'https://zyrafa.itch.io/hryvnia-stories' },
+    { name: 'Retake Crimea', img: 'https://img.itch.zone/aW1nLzk4ODc4ODEucG5n/315x250%23c/2vFt0l.png', url: 'https://africacrossgames.itch.io/retake-crimea' },
+    { name: 'Saint Javelin', img: 'https://img.itch.zone/aW1nLzg4NDkxNzUucG5n/315x250%23c/gBRdb5.png', url: 'https://anomalousfrog.itch.io/saint-javelin' },
+    { name: '300', img: 'https://img.itch.zone/aW1nLzg3NDM3NTMucG5n/315x250%23c/ukEVIN.png', url: 'https://lunariusis.itch.io/300' },
+    { name: 'No to war in Ukraine', img: 'https://img.itch.zone/aW1nLzgzNzA5NzkuZ2lm/315x250%23cm/4jwkGe.gif', url: 'https://boticelly.itch.io/no-to-war-in-ukraine' },
+    { name: 'Ghost of Ukraine', img: 'https://img.itch.zone/aW1nLzkzOTEzNDYucG5n/315x250%23c/n7VH%2Bg.png', url: 'https://staryshko.itch.io/ghost-of-ukraine' },
+    { name: 'Cinque Terre', img: 'https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=', url: 'https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=' }
+];
